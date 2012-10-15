2f8b6e57723fe755bab2444c4ccb19c3852fe298
Guillaume Girou <girou.g@sfeir.com>
2012-10-15 23:04:58 +0200

    Updated mirror and unittest

--- mirrors/0_mirrors.dart
+++ mirrors/0_mirrors.dart
@@ -5,4 +5,9 @@ main(){
   var message = new Message("Hello");
   message.render();
   // TODO le même résulat avec des mirroirs
+  
+  // TODO Get the current mirror system
+  // TODO Get the message_lib library
+  // TODO Get the message class
+  // TODO Create a new instance of Message and invoke the render method 
 }
--- unittest/0_start/test/add_operation_test.dart
+++ unittest/0_start/test/add_operation_test.dart
@@ -11,9 +11,19 @@ main() {
 
   // TODO: test addToStream with writeString that throws FileIOException then always return true
   test('test addToStream', () {
+    // TODO: instantiate mock
+    // TODO: when call to writeString, first throw then return true
+    
+    // TODO: call addToStream
+    
+    // TODO: in logs, with calls to writeString("toto"), verify that happened exactly 3 times 
+    // TODO: in logs, verify nothing else never happened 
   });
 
   // TODO: test asyncAddNumeric method
   test('test asyncAddNumeric', () {
+    // TODO: create a callback that expect the result to equals -3.5
+
+    // TODO: call the asyncAddNumeric method expecting an async call to callback
   });
 }
